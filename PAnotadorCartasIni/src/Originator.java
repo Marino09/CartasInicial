53da08f936bed9d6d182475f0ef229c4d118827f
marino <me.martinezp9@gmail.com>
2013-08-26 14:48:43 -0400

    Originator class Completed

--- PAnotadorCartasIni/src/Originator.java
+++ PAnotadorCartasIni/src/Originator.java
@@ -5,7 +5,22 @@ public class Originator {
 	
 		public void set(String newArticle)
 		{
-			System.out.println("");
+			System.out.println("Originator: Version actual del articulo\n"
+					+ newArticle+ "]n");
+			article = newArticle;
+		}
+		
+		public Memento storeInMemento()
+		{
+			System.out.println("Originator: Salvando memento.");
+			return new Memento(article);
+		}
+		
+		public String storeFromMemento(Memento memento)
+		{
+			article  = memento.getSavedArticle();
+			System.out.println("Originator: Version anterior del articulo");
+			return article;
 		}
 
 }

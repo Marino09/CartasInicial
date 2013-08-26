b2f999af60a4a00da0c5938a62f81d9280fe6055
marino <me.martinezp9@gmail.com>
2013-08-26 12:54:45 -0400

    Memento Class Completed

--- PAnotadorCartasIni/src/Memento.java
+++ PAnotadorCartasIni/src/Memento.java
@@ -2,4 +2,14 @@
 public class Memento {
 	//Memento Class.
 	private String article;
+	
+	public Memento(String articleSave)
+	{
+		article = articleSave;
+	}
+	
+	public String getSavedArticle()
+	{
+		return article;
+	}
 }

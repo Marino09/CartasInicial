85396cd529a5c2d3afa9566ba544cb60c5166397
ernest21 <ernest2193@gmail.com>
2013-09-01 23:17:34 -0400

    Add linkedList

--- PAnotadorCartasIni/src/Hand.java
+++ PAnotadorCartasIni/src/Hand.java
@@ -1,5 +1,26 @@
 
 public class Hand {
 	
+	private String handGame;
+	private int pointHand;
+	private boolean valida = true;
+	
+	
+	public String getHandGame() {
+		return handGame;
+	}
+
+	public int getPointHand() {
+		return pointHand;
+	}
+
+	public boolean isValida() {
+		return valida;
+	}
+
+	public void setValida(boolean valida) {
+		this.valida = valida;
+	}
+	
 	
 }
---
+++ PAnotadorCartasIni/src/Node.java
@@ -0,0 +1,36 @@
+
+public class Node {
+	
+	private Hand hand;
+	private Node next;
+	private Node prev;
+	
+	
+	public Node(Hand hand, Node next, Node prev) {
+		this.hand = hand;
+		this.next = next;
+		this.prev = prev;
+	}
+	
+	public Hand getHand() {
+		return hand;
+	}
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Node getPrev() {
+		return prev;
+	}
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+	
+	
+
+}

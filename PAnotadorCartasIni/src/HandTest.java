2c9ce94eb5477a4bd7e225de6aa117d57ebb0900
Ernest <ernest2193@gmail.com>
2013-10-16 11:38:06 -0400

    add Test fin

--- PAnotadorCartasIni/src/HandTest.java
+++ PAnotadorCartasIni/src/HandTest.java
@@ -28,12 +28,29 @@ public class HandTest {
 		TestHand = new Hand("AH 3D 10S 8C","4D KS 8D AD 3H 5H 10H ");
 		assertTrue(TestHand.isValida());
 		
-		
-		
 	}
 	
 	@Test
 	public void scoreTest() {
+		Hand TestHand;
+		
+		TestHand = new Hand(" 3 fd ","4D KS 8D AD 3H 5H 10H AH");
+		assertTrue(TestHand.getScoreHand() == 0);
+		
+		TestHand = new Hand("6H 7H 8H 9H","4D KS 8D AD 3H 5H 10H AH");
+		assertTrue(TestHand.getScoreHand() == 500);
+		
+		TestHand = new Hand("4H 5S 6S 7H","4D KS 8D AD 3H 5H 10H AH");
+		assertTrue(TestHand.getScoreHand() == 300);
+		
+		TestHand = new Hand("QS QC QH QD","4D KS 8D AD 3H 5H 10H AH");
+		assertTrue(TestHand.getScoreHand() == 100);
+		
+		TestHand = new Hand("4H 5D 6S 7H","4D KS 8D AD 3H 5H 10H AH");
+		assertTrue(TestHand.getScoreHand() == 10);
+		
+		TestHand = new Hand("4H 8C 10D AC","4D KS 8D AD 3H 5H 10H AH");
+		assertTrue(TestHand.getScoreHand() == -10);
 		
 	}
 
